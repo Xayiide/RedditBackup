a2361fd29f1dda71d83214067d36e0b30a490e56
Cheerio <wickedcheerio@gitkraken.com>
2020-02-10 00:37:24 -0600

    Created the skeleton of the project

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
 import praw
+import utils
+
+
+
+def main():
+
+    reddit = praw.Reddit('Backer')
+
+
+    # Obtain an instance of myself
+    # Obtain all the subs I've joined
+    # Obtain all the multis I've joined or created
+    # Obtain all my saved posts
+    me = reddit.user.me()
+    
+    mymultis = me.multireddits()
+    for multi in mymultis:
+        print(multi)
+
+
+
+if __name__ == '__main__':
+    main()
